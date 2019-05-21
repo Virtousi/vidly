07d67ee57a84364f4d20190be54a0c944bb46a75
P1320287 <P1320287@NCS-041018JJ01>
2019-05-21 13:56:31 +0800

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customer = GetCustomers();
+            return View(customer);
+        }
+
+        public ViewResult Details(int ID)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.ID == ID);
+            return View(customer);
+        }
+
+        private IEnumerable<Customers> GetCustomers()
+        {
+            return new List<Customers>
+            {
+                new Customers {ID = 1, Name = "John Smith" },
+                new Customers {ID = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
