64e2469e0ad3e160caa43092b2948ca88bf77afc
P1320287 <P1320287@NCS-041018JJ01>
2019-05-21 14:08:08 +0800

    Testing changes

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -25,6 +25,8 @@ namespace Vidly.Controllers
 
         private IEnumerable<Customers> GetCustomers()
         {
+            var number = 1;
+            number = number + 1;
             return new List<Customers>
             {
                 new Customers {ID = 1, Name = "John Smith" },
