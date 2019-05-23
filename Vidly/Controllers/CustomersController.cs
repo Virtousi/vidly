1ce5fc880053544276499863989dec640010666d
P1320287 <P1320287@NCS-041018JJ01>
2019-05-23 16:06:08 +0800

    1. Include Foreign Key to link Customers and Membership type table. 2. Insert/Updates values for Customers and Memberships 3.Include migrations 4.Change index page of Customers to show discount and type of memberships values

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -10,26 +11,31 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ViewResult Index()
         {
-            var customer = GetCustomers();
+            //include to instant the objects into the html webpage
+            //get the values from MembershipType Tables
+            var customer = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customer);
         }
 
         public ViewResult Details(int ID)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.ID == ID);
+            var customer = _context.Customers.SingleOrDefault(c => c.ID == ID);
             return View(customer);
         }
 
-        private IEnumerable<Customers> GetCustomers()
-        {
-            return new List<Customers>
-            {
-                new Customers {ID = 1, Name = "John Smith" },
-                new Customers {ID = 2, Name = "Mary Williams" }
-            };
-        }
     }
 }
--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -8,6 +8,9 @@ namespace Vidly.Models
     public class MembershipType
     {
         public byte ID { get; set; }
+        //Excercise 3.1 (Create reference)
+        //go to package manager, add-migration AddMemberShipName, Update-database
+        public string MembershipName { get; set; }
         public short SignupFree { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
